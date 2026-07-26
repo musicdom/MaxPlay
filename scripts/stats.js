c21adfab3f132989e61dfb54b8d7c8f943f6daf7
max_digital <sasha12071998214@gmail.com>
2026-07-26 23:13:43 +1000

    Update stats.js

--- scripts/stats.js
+++ scripts/stats.js
@@ -1,38 +1,26 @@
-import CONFIG from './config.js';
-import Cache from './cache.js';
-
-const Stats = {
-    getStats() {
+// Глобальная статистика
+var Stats = {
+    getStats: function() {
         return Cache.load(CONFIG.STATS_KEY) || {};
     },
-
-    saveStats(stats) {
+    saveStats: function(stats) {
         Cache.save(CONFIG.STATS_KEY, stats);
     },
-
-    recordView(offerId) {
-        const stats = this.getStats();
-        if (!stats[offerId]) {
-            stats[offerId] = { views: 0, clicks: 0, favorites: 0 };
-        }
+    recordView: function(offerId) {
+        var stats = this.getStats();
+        if (!stats[offerId]) stats[offerId] = { views: 0, clicks: 0, favorites: 0 };
         stats[offerId].views = (stats[offerId].views || 0) + 1;
         this.saveStats(stats);
     },
-
-    recordClick(offerId) {
-        const stats = this.getStats();
-        if (!stats[offerId]) {
-            stats[offerId] = { views: 0, clicks: 0, favorites: 0 };
-        }
+    recordClick: function(offerId) {
+        var stats = this.getStats();
+        if (!stats[offerId]) stats[offerId] = { views: 0, clicks: 0, favorites: 0 };
         stats[offerId].clicks = (stats[offerId].clicks || 0) + 1;
         this.saveStats(stats);
     },
-
-    recordFavorite(offerId, isAdded) {
-        const stats = this.getStats();
-        if (!stats[offerId]) {
-            stats[offerId] = { views: 0, clicks: 0, favorites: 0 };
-        }
+    recordFavorite: function(offerId, isAdded) {
+        var stats = this.getStats();
+        if (!stats[offerId]) stats[offerId] = { views: 0, clicks: 0, favorites: 0 };
         if (isAdded) {
             stats[offerId].favorites = (stats[offerId].favorites || 0) + 1;
         } else {
@@ -40,16 +28,12 @@ const Stats = {
         }
         this.saveStats(stats);
     },
-
-    getOfferStats(offerId) {
-        const stats = this.getStats();
+    getOfferStats: function(offerId) {
+        var stats = this.getStats();
         return stats[offerId] || { views: 0, clicks: 0, favorites: 0 };
     },
-
-    getPopularity(offerId) {
-        const stats = this.getOfferStats(offerId);
-        return stats.views * 1 + stats.clicks * 3 + stats.favorites * 5;
+    getPopularity: function(offerId) {
+        var s = this.getOfferStats(offerId);
+        return s.views + s.clicks * 3 + s.favorites * 5;
     }
 };
-
-export default Stats;
